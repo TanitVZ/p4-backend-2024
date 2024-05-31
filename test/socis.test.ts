206b50381fe53f84131301db2bddfbe5f30b93f6
Laia Valenti <laia.valenti@gmail.com>
2024-05-31 17:37:20 +0200

    Prova test d'API

--- test/socis.test.ts
+++ test/socis.test.ts
@@ -1,8 +1,29 @@
-import { expect, test, vi } from 'vitest' // 👈🏻 Added the `vi` import
-import router from '../src/socis'
+import { expect, test, vi } from "vitest"; // 👈🏻 Added the `vi` import
+import { db } from "../src/db";
 
 
-test("1===1", () => {
-  expect(1).toBe(1);
+test("crear soci retorna id del soci i id de la quota", async () => {
+  const nouSoci = {
+    data: {
+      nom: "Laura",
+      cognoms: "Perez Alonso",
+      dni: "74945682K",
+      email: "lpa@gmail.com",
+
+      quotaSoci: {
+        create: {
+          quantitat: 50,
+          iban: "ES7220953581368561352669",
+          quotaId: 3,
+        },
+      },
+    },
+    include: {
+      quotaSoci: true,
+    },
+  };
+  const soci = await db.soci.create(nouSoci);
+  expect(soci.sociId).toBeDefined();
+  expect(soci.quotaSoci?.quotaId).toBeDefined();
+  expect(soci.actiu).toBe(true);
 });
-
